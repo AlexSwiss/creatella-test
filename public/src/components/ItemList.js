845db36e1660ee6abadadf1ca2309e7a65d09b30
AlexSwiss <39837597+AlexSwiss@users.noreply.github.com>
2019-08-10 12:44:53 +0100

    finished arranging products in grid and adding a little style

--- public/src/components/ItemList.js
+++ public/src/components/ItemList.js
@@ -1,36 +1,59 @@
 import React, { Component } from 'react';
+import { Container, Row, Col, Button, Card } from 'reactstrap';
+
 
 class itemList extends Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
-            products: []
-        };
+            'products': []
+        }
     }
 
     componentDidMount() {
-        let url = "http://localhost:3000/api/products"
+        this.getProducts();
+    }
+
+    getProducts() {
+        let url = "http://localhost:3000/api/products/"
         fetch(url)
             .then(res => res.json())
-            .then(data => {
-                let products = data.map((product, id) => {
-                    return (
-                        <div key={id}>
-                            <h3>{product.face}</h3>
-                            <h3>{product.size}</h3>
-                            <h3>{product.price}</h3>
-                            <h3>{product.date}</h3>
-                        </div>
-                    )
-                })
-                    this.setState({products: products});
-            })
+            .then(res => this.setState({'products': res}));
     }
     render() {
-        return (
-            <div className="App">
-                {this.state.products}
-            </div>
+        return(
+            <Container>
+                <Row>
+                    <Col>
+                    {this.state.products.map(function(product, id) {
+                    return (
+                        <Card body outline color="success">
+                            <Container>
+                                <Row>
+                                    <Col>
+                                        <h3>{product.face}</h3>
+                                    </Col>
+                                    <Col>
+                                        <h5><span>Price:</span> ${product.price}</h5>
+                                    </Col>
+                                    <Col>
+                                        <h5><span>Size</span> {product.size} px </h5>
+                                    </Col>
+                                    <Col>
+                                        {product.date}
+                                    </Col>
+                                    <Col>
+                                        <Button color="success">Buy</Button>                                        
+                                    </Col>
+                                </Row>
+                            </Container>
+                            </Card>
+                                                )
+                            }
+                        )}
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
