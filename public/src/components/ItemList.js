1972be2da93ee5376f37c330e7d84d26992297a1
AlexSwiss <39837597+AlexSwiss@users.noreply.github.com>
2019-08-14 12:10:16 +0100

    finished the infinite scrolling of pages

--- public/src/components/ItemList.js
+++ public/src/components/ItemList.js
@@ -1,28 +1,74 @@
 import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
 import { Container, Row, Col, Button, Card } from 'reactstrap';
 
 
-class itemList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            'products': []
-        }
-    }
+class ItemList extends Component {
 
-    componentDidMount() {
-        this.getProducts();
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+        products: [],
+        limit: 20,
+        page: 1,
+        total_pages: null,
 
-    getProducts() {
-        let url = "http://localhost:3000/api/products/"
-        fetch(url)
-            .then(res => res.json())
-            .then(res => this.setState({'products': res}));
+    };
+}
+
+loadUser () {
+  const { limit, page, products, sort } = this.state;
+  const url = `http://localhost:3000/api/products?_page=${page}&_limit=${limit}`;
+     fetch(url)
+    .then(res => res.json())
+    .then(res =>
+     this.setState({
+          products: [...products, ...res],
+          scrolling: false,
+          total_pages: products.total_pages
+    })
+ );
+}
+
+
+loadMore() {
+  this.setState(
+    prevState => ({
+      page: prevState.page + 1,
+      scrolling: true
+    }),
+    this.loadUser
+  );
+};
+
+handleScroll(){ 
+  var lastLi = document.querySelector("ul.container > li:last-child");
+  var lastLiOffset = lastLi.offsetTop + lastLi.clientHeight;
+  var pageOffset = window.pageYOffset + window.innerHeight;
+if (pageOffset > lastLiOffset) {
+       this.loadMore();
+  }
+};
+
+componentWillMount() {
+  this.loadUser();
+}
+
+componentDidMount() {
+  this.refs.iScroll.addEventListener("scroll", () => {
+    if (this.refs.iScroll.scrollTop + this.refs.iScroll.clientHeight >=this.refs.iScroll.scrollHeight){
+      this.loadMore();
     }
-    render() {
-        return(
-            <Container>
+  });
+}
+  
+  render() {
+    return (
+      <div
+      ref="iScroll"
+      style={{ height: "600px", overflow: "auto" }}
+      >
+      <Container>
                 <Row>
                     <Col>
                     {this.state.products.map(function(product, id) {
@@ -47,16 +93,17 @@ class itemList extends Component {
                                     </Col>
                                 </Row>
                             </Container>
-                            </Card>
-                                                )
+                        </Card>
+                            
+                                );
                             }
                         )}
                     </Col>
                 </Row>
             </Container>
-        );
-    }
+<p><h4>Loading...</h4></p>
+    </div>  
+    );
+  }
 }
-
-
-export default itemList;
+export default ItemList;
